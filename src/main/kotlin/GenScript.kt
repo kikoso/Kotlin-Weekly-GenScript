ad120f413ad7dc5c0f748b5cf1d348a069ede67c
Enrique López Mañas <eenriquelopez@gmail.com>
2020-01-02 14:02:53 +0700

    Added colors

--- src/main/kotlin/GenScript.kt
+++ src/main/kotlin/GenScript.kt
@@ -19,7 +19,7 @@ fun main() {
     result.videos?.let { html += createVideos(result.videos) }
     result.jobs?.let { html += createJobs(result.jobs) }
     result.podcast?.let { html += createPodcast(result.podcast) }
-    result.conferences?.let { html += createPodcast(result.conferences) }
+    result.conferences?.let { html += createConferences(result.conferences) }
 
     html += createFooter()
     print(html)
@@ -82,10 +82,10 @@ fun createArticles(articles: List<Item>): String {
             "    \"><br />\n"
 
     for (article in articles) {
-        html += "<a href=\"" + article.link + "\" style=\"text-decoration:none\" target=\"_blank\"><span style=\"font-size:16px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><strong><span style=\"color:#"+Colors.ARTICLES+"\">" + article.text + "</span></strong></span></span></a><br />\n" +
+        html += "<a href=\"" + article.link + "\" style=\"text-decoration:none\" target=\"_blank\"><span style=\"font-size:16px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><strong><span style=\"color:"+Colors.ARTICLES+"\">" + article.text + "</span></strong></span></span></a><br />\n" +
                 "" + article.description + "<br />"
         var url = URL(article.link)
-        html += "<a href=\"" + url.host + "\" style=\"text-decoration:none\" target=\"_blank\"><strong><span style=\"color:#"+Colors.ARTICLES+">" + url.host + "</span></strong></a><br />\n" +
+        html += "<a href=\"" + url.host + "\" style=\"text-decoration:none\" target=\"_blank\"><strong><span style=\"color:"+Colors.ARTICLES+"\">" + url.host + "</span></strong></a><br />\n" +
                 "<br />"
     }
     html += "</div></div>"
@@ -111,10 +111,10 @@ fun createAndroid(articles: List<Item>): String {
                 "    width:70%;\n" +
                 "    float:left;\n" +
                 "    \"><br />\n" +
-                "<a href=\"" + article.link + "\" style=\"text-decoration:none\" target=\"_blank\"><span style=\"font-size:16px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><strong><span style=\"color:#79c5b4\">" + article.text + "</span></strong></span></span></a><br />\n" +
+                "<a href=\"" + article.link + "\" style=\"text-decoration:none\" target=\"_blank\"><span style=\"font-size:16px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><strong><span style=\"color:"+Colors.ANDROID+"\">" + article.text + "</span></strong></span></span></a><br />\n" +
                 "" + article.description + "<br />"
         val url = URL(article.link)
-        html += "<a href=\"" + url.host + "\" style=\"text-decoration:none\" target=\"_blank\"><strong><span style=\"color:#79c5b4\">" + url.host + "</span></strong></a><br />\n" +
+        html += "<a href=\"" + url.host + "\" style=\"text-decoration:none\" target=\"_blank\"><strong><span style=\"color:"+Colors.ANDROID+"\">" + url.host + "</span></strong></a><br />\n" +
                 "<br />"
     }
 
@@ -134,9 +134,9 @@ fun createSponsored(sponsored: Item): String {
     html += "<div style=\"  background-color:#f0f0f0;\n" +
             "    width:66%;\n" +
             "    float:left; padding:5px 5px 5px 10px\n" +
-            "    \"><a href=\""+sponsored.link+"\" style=\"text-decoration:none\" target=\"_blank\"><strong><span style=\"font-size:16px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><span style=\"color:#b3a34f\">"+sponsored.text+"</span></span></span></strong></a><br />\n" +
+            "    \"><a href=\""+sponsored.link+"\" style=\"text-decoration:none\" target=\"_blank\"><strong><span style=\"font-size:16px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><span style=\"color:"+Colors.SPONSORED+"\">"+sponsored.text+"</span></span></span></strong></a><br />\n" +
             ""+sponsored.description+"<br />\n" +
-            "<a href=\""+url.host+"\" style=\"text-decoration:none\" target=\"_blank\"><strong><span style=\"color:#b3a34f\">getstream.io</span></strong></a></div>\n" +
+            "<a href=\""+url.host+"\" style=\"text-decoration:none\" target=\"_blank\"><strong><span style=\"color:"+Colors.SPONSORED+"\">getstream.io</span></strong></a></div>\n" +
             "</div>\n"
     return html
 }
@@ -148,7 +148,7 @@ fun createMultiplatform(articles: List<Item>): String {
             "    float:left\">\n" +
             "<p style=\"text-align: center;\"><br />\n" +
             "<img data-file-id=\"2573205\" height=\"65\" src=\"https://gallery.mailchimp.com/f39692e245b94f7fb693b6d82/images/d6dd1767-fc87-4f29-8899-ab4caae6903d.png\" style=\"border: 0px initial ; width: 65px; height: 65px; margin: 0px;\" width=\"65\" /><br />\n" +
-            "<strong><span style=\"font-size:11px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><span style=\"color:#FFFFFF\"><span style=\"background-color:#79c5b4; padding:5px 5px 5px 5px\">Multiplatform</span></span></span></span></strong></p>\n" +
+            "<strong><span style=\"font-size:11px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><span style=\"color:#FFFFFF\"><span style=\"background-color:"+Colors.KOTLIN_MULTIPLATFORM+"; padding:5px 5px 5px 5px\">Multiplatform</span></span></span></span></strong></p>\n" +
             "</div>"
     html += "<div style=\"  \n" +
             "    width:70%;\n" +
@@ -159,10 +159,10 @@ fun createMultiplatform(articles: List<Item>): String {
                 "    width:70%;\n" +
                 "    float:left;\n" +
                 "    \"><br />\n" +
-                "<a href=\"" + article.link + "\" style=\"text-decoration:none\" target=\"_blank\"><span style=\"font-size:16px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><strong><span style=\"color:#79c5b4\">" + article.text + "</span></strong></span></span></a><br />\n" +
+                "<a href=\"" + article.link + "\" style=\"text-decoration:none\" target=\"_blank\"><span style=\"font-size:16px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><strong><span style=\"color:"+Colors.KOTLIN_MULTIPLATFORM+"\">" + article.text + "</span></strong></span></span></a><br />\n" +
                 "" + article.description + "<br />"
         val url = URL(article.link)
-        html += "<a href=\"" + url.host + "\" style=\"text-decoration:none\" target=\"_blank\"><strong><span style=\"color:#79c5b4\">" + url.host + "</span></strong></a><br />\n" +
+        html += "<a href=\"" + url.host + "\" style=\"text-decoration:none\" target=\"_blank\"><strong><span style=\"color:"+Colors.KOTLIN_MULTIPLATFORM+"\">" + url.host + "</span></strong></a><br />\n" +
                 "<br />"
     }
 
@@ -176,17 +176,17 @@ fun createJobs(articles: List<Item>): String {
             "    width:30%;\n" +
             "    float:left\">\n" +
             "<p style=\"text-align: center;\"><img data-file-id=\"2573181\" height=\"65\" src=\"https://gallery.mailchimp.com/f39692e245b94f7fb693b6d82/images/b3899433-4991-436c-b3ac-f4f309129ed3.jpg\" style=\"border: 0px  ; width: 65px; height: 65px; margin: 0px;\" width=\"65\" /><br />\n" +
-            "<strong><span style=\"font-size:11px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><span style=\"color:#FFFFFF\"><span style=\"background-color:#1a3b47; padding:5px 5px 5px 5px\">Jobs</span></span></span></span></strong></p>\n" +
+            "<strong><span style=\"font-size:11px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><span style=\"color:#FFFFFF\"><span style=\"background-color:"+Colors.JOBS+"; padding:5px 5px 5px 5px\">Jobs</span></span></span></span></strong></p>\n" +
             "</div>"
     for (article in articles) {
         html += "<div style=\"background-color:#f0f0f0;\n" +
                 "    width:70%;\n" +
                 "    float:left;\n" +
                 "    \"><br />\n" +
-                "<a href=\"" + article.link + "\" style=\"text-decoration:none\" target=\"_blank\"><span style=\"font-size:16px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><strong><span style=\"color:#1a3b47\">" + article.text + "</span></strong></span></span></a><br />\n" +
+                "<a href=\"" + article.link + "\" style=\"text-decoration:none\" target=\"_blank\"><span style=\"font-size:16px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><strong><span style=\"color:"+Colors.JOBS+"\">" + article.text + "</span></strong></span></span></a><br />\n" +
                 "" + article.description + "<br />"
         val url = URL(article.link)
-        html += "<a href=\"" + url.host + "\" style=\"text-decoration:none\" target=\"_blank\"><strong><span style=\"color:#1a3b47\">" + url.host + "</span></strong></a><br />\n" +
+        html += "<a href=\"" + url.host + "\" style=\"text-decoration:none\" target=\"_blank\"><strong><span style=\"color:"+Colors.JOBS+"\">" + url.host + "</span></strong></a><br />\n" +
                 "<br />"
     }
 
@@ -201,7 +201,7 @@ fun createVideos(articles: List<Item>): String {
             "    float:left\">\n" +
             "<p style=\"text-align: center;\"><br />\n" +
             "<img data-file-id=\"2573177\" height=\"65\" src=\"https://gallery.mailchimp.com/f39692e245b94f7fb693b6d82/images/85924742-e83e-4289-96e8-4dba7fa1a8a8.png\" style=\"border: 0px  ; width: 65px; height: 65px; margin: 0px;\" width=\"65\" /><br />\n" +
-            "<strong><span style=\"font-size:11px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><span style=\"color:#FFFFFF\"><span style=\"background-color:#cc916e; padding:5px 5px 5px 5px\">Videos</span></span></span></span></strong></p>\n" +
+            "<strong><span style=\"font-size:11px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><span style=\"color:#FFFFFF\"><span style=\"background-color:"+Colors.VIDEO+"; padding:5px 5px 5px 5px\">Videos</span></span></span></span></strong></p>\n" +
             "</div>"
     html += "<div style=\"  \n" +
             "    width:70%;\n" +
@@ -212,10 +212,10 @@ fun createVideos(articles: List<Item>): String {
                 "    width:70%;\n" +
                 "    float:left;\n" +
                 "    \"><br />\n" +
-                "<a href=\"" + article.link + "\" style=\"text-decoration:none\" target=\"_blank\"><span style=\"font-size:16px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><strong><span style=\"color:#cc916e\">" + article.text + "</span></strong></span></span></a><br />\n" +
+                "<a href=\"" + article.link + "\" style=\"text-decoration:none\" target=\"_blank\"><span style=\"font-size:16px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><strong><span style=\"color:"+Colors.VIDEO+"\">" + article.text + "</span></strong></span></span></a><br />\n" +
                 "" + article.description + "<br />"
         val url = URL(article.link)
-        html += "<a href=\"" + url.host + "\" style=\"text-decoration:none\" target=\"_blank\"><strong><span style=\"color:#cc916e\">" + url.host + "</span></strong></a><br />\n" +
+        html += "<a href=\"" + url.host + "\" style=\"text-decoration:none\" target=\"_blank\"><strong><span style=\"color:"+Colors.VIDEO+"\">" + url.host + "</span></strong></a><br />\n" +
                 "<br />"
     }
 
@@ -230,7 +230,7 @@ fun createPodcast(articles: List<Item>): String {
             "    float:left\">\n" +
             "<p style=\"text-align: center;\"><br />\n" +
             "<img data-file-id=\"2573205\" height=\"65\" src=\"https://gallery.mailchimp.com/f39692e245b94f7fb693b6d82/images/d6dd1767-fc87-4f29-8899-ab4caae6903d.png\" style=\"border: 0px initial ; width: 65px; height: 65px; margin: 0px;\" width=\"65\" /><br />\n" +
-            "<strong><span style=\"font-size:11px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><span style=\"color:#FFFFFF\"><span style=\"background-color:#79c5b4; padding:5px 5px 5px 5px\">Podcast</span></span></span></span></strong></p>\n" +
+            "<strong><span style=\"font-size:11px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><span style=\"color:#FFFFFF\"><span style=\"background-color:"+Colors.PODCAST+"; padding:5px 5px 5px 5px\">Podcast</span></span></span></span></strong></p>\n" +
             "</div>"
     html += "<div style=\"  \n" +
             "    width:70%;\n" +
@@ -241,10 +241,10 @@ fun createPodcast(articles: List<Item>): String {
                 "    width:70%;\n" +
                 "    float:left;\n" +
                 "    \"><br />\n" +
-                "<a href=\"" + article.link + "\" style=\"text-decoration:none\" target=\"_blank\"><span style=\"font-size:16px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><strong><span style=\"color:#79c5b4\">" + article.text + "</span></strong></span></span></a><br />\n" +
+                "<a href=\"" + article.link + "\" style=\"text-decoration:none\" target=\"_blank\"><span style=\"font-size:16px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><strong><span style=\"color:"+Colors.PODCAST+"\">" + article.text + "</span></strong></span></span></a><br />\n" +
                 "" + article.description + "<br />"
         val url = URL(article.link)
-        html += "<a href=\"" + url.host + "\" style=\"text-decoration:none\" target=\"_blank\"><strong><span style=\"color:#79c5b4\">" + url.host + "</span></strong></a><br />\n" +
+        html += "<a href=\"" + url.host + "\" style=\"text-decoration:none\" target=\"_blank\"><strong><span style=\"color:"+Colors.PODCAST+"\">" + url.host + "</span></strong></a><br />\n" +
                 "<br />"
     }
 
@@ -259,7 +259,7 @@ fun createConferences(articles: List<Item>): String {
             "    float:left\">\n" +
             "<p style=\"text-align: center;\"><br />\n" +
             "<img data-file-id=\"2573205\" height=\"65\" src=\"https://gallery.mailchimp.com/f39692e245b94f7fb693b6d82/images/d6dd1767-fc87-4f29-8899-ab4caae6903d.png\" style=\"border: 0px initial ; width: 65px; height: 65px; margin: 0px;\" width=\"65\" /><br />\n" +
-            "<strong><span style=\"font-size:11px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><span style=\"color:#FFFFFF\"><span style=\"background-color:#79c5b4; padding:5px 5px 5px 5px\">Podcast</span></span></span></span></strong></p>\n" +
+            "<strong><span style=\"font-size:11px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><span style=\"color:#FFFFFF\"><span style=\"background-color:"+Colors.CONFERENCES+"; padding:5px 5px 5px 5px\">Podcast</span></span></span></span></strong></p>\n" +
             "</div>"
     html += "<div style=\"  \n" +
             "    width:70%;\n" +
@@ -270,10 +270,10 @@ fun createConferences(articles: List<Item>): String {
                 "    width:70%;\n" +
                 "    float:left;\n" +
                 "    \"><br />\n" +
-                "<a href=\"" + article.link + "\" style=\"text-decoration:none\" target=\"_blank\"><span style=\"font-size:16px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><strong><span style=\"color:#79c5b4\">" + article.text + "</span></strong></span></span></a><br />\n" +
+                "<a href=\"" + article.link + "\" style=\"text-decoration:none\" target=\"_blank\"><span style=\"font-size:16px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><strong><span style=\"color:"+Colors.CONFERENCES+"\">" + article.text + "</span></strong></span></span></a><br />\n" +
                 "" + article.description + "<br />"
         val url = URL(article.link)
-        html += "<a href=\"" + url.host + "\" style=\"text-decoration:none\" target=\"_blank\"><strong><span style=\"color:#79c5b4\">" + url.host + "</span></strong></a><br />\n" +
+        html += "<a href=\"" + url.host + "\" style=\"text-decoration:none\" target=\"_blank\"><strong><span style=\"color:"+Colors.CONFERENCES+"\">" + url.host + "</span></strong></a><br />\n" +
                 "<br />"
     }
 
