152964fa7dc5f90462eb2bc74b175f26e65de121
Enrique López Mañas <eenriquelopez@gmail.com>
2020-01-04 14:38:44 +0700

    Change image source for videos

--- src/main/kotlin/GenScript.kt
+++ src/main/kotlin/GenScript.kt
@@ -309,7 +309,7 @@ fun createVideos(articles: List<Item>): String {
             "    width:30%;\n" +
             "    float:left\">\n" +
             "<p style=\"text-align: center;\"><br />\n" +
-            "<img data-file-id=\"2573177\" height=\"65\" src=\"https://gallery.mailchimp.com/f39692e245b94f7fb693b6d82/images/85924742-e83e-4289-96e8-4dba7fa1a8a8.png\" style=\"border: 0px  ; width: 65px; height: 65px; margin: 0px;\" width=\"65\" /><br />\n" +
+            "<img data-file-id=\"2573213\" height=\"45\" src=\"https://gallery.mailchimp.com/f39692e245b94f7fb693b6d82/images/1190a3e2-9a27-4e7b-851e-20983326e25f.png\" style=\"border: 0px initial ; width: 45px; height: 45px; margin: 0px;\" width=\"45\" /><br />" +
             "<strong><span style=\"font-size:11px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><span style=\"color:#FFFFFF\"><span style=\"background-color:" + Colors.VIDEO + "; padding:5px 5px 5px 5px\">Videos</span></span></span></span></strong></p>\n" +
             "</div>"
     html += "<div style=\"  \n" +
