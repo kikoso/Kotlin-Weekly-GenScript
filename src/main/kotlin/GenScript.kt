e8cd0919c4b3ac5eee76761851b869ca9ba4ecb7
kikoso <eenriquelopez@gmail.com>
2020-01-19 04:47:44 +0100

    Changed image for conferences

--- src/main/kotlin/GenScript.kt
+++ src/main/kotlin/GenScript.kt
@@ -361,7 +361,7 @@ fun createConferences(articles: List<Item>): String {
             "    width:30%;\n" +
             "    float:left\">\n" +
             "<p style=\"text-align: center;\"><br />\n" +
-            "<img data-file-id=\"2573205\" height=\"45\" src=\"https://gallery.mailchimp.com/f39692e245b94f7fb693b6d82/images/d6dd1767-fc87-4f29-8899-ab4caae6903d.png\" style=\"border: 0px initial ; width: 45px; height: 45px; margin: 0px;\" width=\"45\" /><br />\n" +
+            "<img data-file-id=\"2573221\" height=\"45\" src=\"https://gallery.mailchimp.com/f39692e245b94f7fb693b6d82/images/d6e8808c-6fdc-4d79-bb44-b92ea4f9b47a.png\" style=\"border: 0px initial ; width: 45px; height: 45px; margin: 0px;\" width=\"45\" /><br />\n" +
             "<strong><span style=\"font-size:11px\"><span style=\"font-family:helvetica neue,helvetica,arial,verdana,sans-serif\"><span style=\"color:#FFFFFF\"><span style=\"background-color:" + Colors.CONFERENCES + "; padding:5px 5px 5px 5px\">Conferences</span></span></span></span></strong></p>\n" +
             "</div>"
     html += "<div style=\"  \n" +
